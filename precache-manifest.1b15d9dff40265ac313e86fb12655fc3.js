8c78eeefc96a4909203751f21875883fd91a9b5a
Patricia <pattricianunes@gmail.com>
2021-06-17 09:19:32 +0200

    Updates

---
+++ precache-manifest.1b15d9dff40265ac313e86fb12655fc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93a67fc9369cfda157798957473924c",
+    "url": "/gyfted-frontend/index.html"
+  },
+  {
+    "revision": "c69cd5e62609256e6e0e",
+    "url": "/gyfted-frontend/static/js/2.4353b899.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gyfted-frontend/static/js/2.4353b899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09859e5536827b2f937d",
+    "url": "/gyfted-frontend/static/js/main.a3c0d5f0.chunk.js"
+  },
+  {
+    "revision": "7b22685e6d30a264bc39",
+    "url": "/gyfted-frontend/static/js/runtime-main.07fe23ed.js"
+  }
+]);
